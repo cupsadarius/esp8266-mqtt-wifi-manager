8e2eae5197feb6c0f072bcf6a97ce48f19088baa
Cupsa Darius <cupsadarius@gmail.com>
2017-08-17 09:34:08 +0300

    add comments

---
+++ README.md
@@ -0,0 +1,7 @@
+# esp8266-mqtt-wifi-manager
+
+
+## Required libraries:
+* [WiFiManager](https://github.com/tzapu/WiFiManager)
+* [ArduinoJson](https://github.com/bblanchon/ArduinoJson)
+* [PubSubClient](https://github.com/knolleary/pubsubclient)
--- src/sketch.ino
+++ src/sketch.ino
@@ -6,27 +6,28 @@
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 
+// global variables which hold the mqtt server credentials and a random generated device id
+// which will be populated from config.json if exists or by the user in the config portal
 char mqtt_server[40];
 char mqtt_port[6];
 char mqtt_user[30];
 char mqtt_pass[30];
 String device_uid = "device-" + String(random(0xffff), HEX);
 
+// instance of MQTT client
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-long lastMsg = 0;
-char msg[50];
-int value = 0;
-
+// flag for knowing if the new config should be saved or not
 bool shouldSaveConfig = false;
 
+// callback used by the wifi manager to announce that the config changed
 void saveConfigCallback () {
   Serial.println("Should save config");
   shouldSaveConfig = true;
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void handleIncommingMessage(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
@@ -47,69 +48,80 @@ void callback(char* topic, byte* payload, unsigned int length) {
 }
 
 void setup() {
+  // initialize serial communication
   Serial.begin(115200);
   Serial.println();
 
-  //clean FS, for testing
+  // format flash storage, used when testing to not store the config between reboots
   SPIFFS.format();
 
-  //read configuration from FS json
-  Serial.println("mounting FS...");
 
-  if (SPIFFS.begin()) {
+  Serial.println("mounting FS...");
+  // reads the contents of config.json if exists and populates the global variables mentioned above
+  if (SPIFFS.begin()) { // open flash storage
     Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
+    if (SPIFFS.exists("/config.json")) { // check if config.json exists
       Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
+      File configFile = SPIFFS.open("/config.json", "r"); // read contents of config.json
+      if (configFile) { /// check if file not empty
         Serial.println("opened config file");
+        // -- begin conversion of json to array --
         size_t size = configFile.size();
         std::unique_ptr<char[]> buf(new char[size]);
-
         configFile.readBytes(buf.get(), size);
         DynamicJsonBuffer jsonBuffer;
         JsonObject& json = jsonBuffer.parseObject(buf.get());
         json.printTo(Serial);
-        if (json.success()) {
+        // -- end conversion --
+        if (json.success()) { // check if conversion was successful
           Serial.println("\nparsed json");
 
-          strcpy(mqtt_server, json["mqtt_server"]);
-          strcpy(mqtt_port, json["mqtt_port"]);
-          strcpy(mqtt_user, json["mqtt_user"]);
-          strcpy(mqtt_pass, json["mqtt_pass"]);
-          device_uid = json["device_uid"].asString();
+          strcpy(mqtt_server, json["mqtt_server"]); // populate mqtt_server from config
+          strcpy(mqtt_port, json["mqtt_port"]); // populate mqtt_port from config
+          strcpy(mqtt_user, json["mqtt_user"]); // populate mqtt_user from config
+          strcpy(mqtt_pass, json["mqtt_pass"]); // populate mqtt_pass from config
+          device_uid = json["device_uid"].asString(); // populate device id from config
 
-        } else {
+        } else { // json conversion failed
           Serial.println("failed to load json config");
         }
       }
     }
-  } else {
+  } else { // failed to mount flash storage
     Serial.println("failed to mount FS");
   }
-  //end read
 
+  // -- begin configuration of the portal --
+
+  // create custom parameters for the portal
   WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
   WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
   WiFiManagerParameter custom_mqtt_user("user", "mqtt user", mqtt_port, 30);
   WiFiManagerParameter custom_mqtt_pass("pass", "mqtt password", mqtt_pass, 30);
 
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
+  // WiFiManager
+  // Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
 
-  //set config save notify callback
+  // set the callback used for notifying if there was a change
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
-  //add all your parameters here
+  // add custom parameters to portal§
   wifiManager.addParameter(&custom_mqtt_server);
   wifiManager.addParameter(&custom_mqtt_port);
   wifiManager.addParameter(&custom_mqtt_user);
   wifiManager.addParameter(&custom_mqtt_pass);
 
-  //reset settings - for testing
+  // reset portal settings. used for testing only. to force opening the portal on each boot.
   wifiManager.resetSettings();
 
+  // if the board could not connect to wifi on the first boot.
+  // an access point called Configure Device would be created
+  // when connecting to this access point a configuration portal would be displayed
+  // where the user can set a new SSID and password for connecting to wifi
+  // and the required credentials for mqtt.
+  // this will run in a blocking loop so the boot process would stay here as long
+  // as the board is not connected to wifi.
   if (!wifiManager.autoConnect("Configure Device")) {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
@@ -118,18 +130,21 @@ void setup() {
     delay(5000);
   }
 
+  // -- end configuration --
+
   //if you get here you have connected to the WiFi
   Serial.println("connected!");
 
+  // if a new config was set in the portal, it will get saved as json on the flash storage.
   if (shouldSaveConfig) {
-    //read updated parameters
+
+    // populate mqtt_server, mqtt_port, mqtt_user, mqtt_pass with the information provided by the user.
     strcpy(mqtt_server, custom_mqtt_server.getValue());
     strcpy(mqtt_port, custom_mqtt_port.getValue());
     strcpy(mqtt_user, custom_mqtt_user.getValue());
     strcpy(mqtt_pass, custom_mqtt_pass.getValue());
 
-    //save the custom parameters to FS
-
+    // -- begin conversion of parameters to json and save them to flash storage --
     Serial.println("saving config");
     DynamicJsonBuffer jsonBuffer;
     JsonObject& json = jsonBuffer.createObject();
@@ -148,28 +163,34 @@ void setup() {
     json.printTo(Serial);
     json.printTo(configFile);
     configFile.close();
-    //end save
+
+    // -- end conversion --
   }
 
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
 
+  // in the scope of this demo we will control the built in led on the boards.
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
 
-  client.setServer(mqtt_server, atoi(mqtt_port));
-  client.setCallback(callback);
-
+  // -- begin mqtt configuration --
+  client.setServer(mqtt_server, atoi(mqtt_port)); // connect to host and port
+  client.setCallback(handleIncommingMessage); // set handler for incomming messages on subscribed channels
+  // -- end mqtt configuration --
 }
 
+// method used to reconnect the mqtt client
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
 
-    // Attempt to connect
+    // connect to mqtt server with user and password and pass the device id,
+    // device id should be unique on the bus so pay attention to this.
     if (client.connect(device_uid.c_str(), mqtt_user, mqtt_pass)) {
       Serial.println("connected");
-      // ... and resubscribe
+
+      // subscribe to what channels you want to listen to.
       client.subscribe("action");
     } else {
       Serial.print("failed, rc=");
@@ -179,16 +200,17 @@ void reconnect() {
       delay(5000);
     }
   }
+  // publish to the bus, a message used for auto discovery.
   Serial.println(("Device connected " + device_uid).c_str());
-  client.publish("connected", "Connected");
   client.publish("discover", ("Device connected " + device_uid).c_str());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // as long as the mqtt client is not connected, reconnect.
   if (!client.connected()) {
       reconnect();
   }
+  // on each iteration of loop, mqtt client would check the bus for new messages
   client.loop();
 
 }
